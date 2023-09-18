35b372c1b8c4138606b1c9088d2b82c966b915c0
Ingrid Kamga <ingrid.kamga@clemios.com>
2023-09-18 18:08:49 +0100

    Generate ed25519 did:key addresses

--- did-utils/src/methods/did_key/method.rs
+++ did-utils/src/methods/did_key/method.rs
@@ -8,6 +8,7 @@ use crate::{
     methods::traits::DIDMethod,
 };
 
+#[allow(unused)]
 #[non_exhaustive]
 pub enum Algorithm {
     Ed25519,
@@ -15,19 +16,22 @@ pub enum Algorithm {
 }
 
 impl Algorithm {
-    pub fn muticodec_prefix(&self) -> Vec<u8> {
+    pub fn muticodec_prefix(&self) -> [u8; 2] {
         match self {
-            Self::Ed25519 => vec![0xed],
-            Self::X25519 => vec![0xec],
+            Self::Ed25519 => [0xed, 0x01],
+            Self::X25519 => [0xec, 0x01],
         }
     }
 }
 
+#[derive(Default)]
 pub enum PublicKeyFormat {
+    #[default]
     Multikey,
     Jwk,
 }
 
+#[derive(Default)]
 pub struct DIDKeyMethod {
     /// Key format to consider during DID
     /// expansion into a DID document.
@@ -42,13 +46,27 @@ impl DIDMethod for DIDKeyMethod {
 
 impl DIDKeyMethod {
     /// Generates a did:key address ex-nihilo
-    pub fn generate() -> Result<String, CryptoError> {
+    pub fn generate(&self) -> Result<String, CryptoError> {
         let keypair = Ed25519KeyPair::new()?;
         let multibase_value = multibase::encode(
             Base58Btc,
-            [Algorithm::Ed25519.muticodec_prefix().as_slice(), &keypair.public_key_bytes()?].concat(),
+            [&Algorithm::Ed25519.muticodec_prefix(), keypair.public_key_bytes()?.as_slice()].concat(),
         );
 
         Ok(format!("did:key:{}", multibase_value))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_did_key_generation() {
+        let did_method = DIDKeyMethod::default();
+        let did = did_method.generate();
+
+        assert!(did.is_ok());
+        assert!(did.unwrap().starts_with("did:key:z6Mk"));
     }
 }
