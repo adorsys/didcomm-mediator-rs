ba62fe7d5a1969f6baa67d508fb8b03df8219e5e
Ingrid Kamga <ingrid.kamga@clemios.com>
2023-09-19 13:11:28 +0100

    Implement did:key expansion without key agreement derivation

--- did-utils/src/methods/did_key/method.rs
+++ did-utils/src/methods/did_key/method.rs
@@ -5,7 +5,9 @@ use crate::{
         ed25519::Ed25519KeyPair,
         traits::{Error as CryptoError, Generate, KeyMaterial},
     },
-    methods::traits::DIDMethod,
+    didcore::{self, Document as DIDDocument, KeyFormat, VerificationMethod},
+    ldmodel::Context,
+    methods::{errors::DIDResolutionError, traits::DIDMethod},
 };
 
 use super::alg::Algorithm;
@@ -19,9 +21,11 @@ pub enum PublicKeyFormat {
 
 #[derive(Default)]
 pub struct DIDKeyMethod {
-    /// Key format to consider during DID
-    /// expansion into a DID document.
+    /// Key format to consider during DID expansion into a DID document
     pub key_format: PublicKeyFormat,
+
+    /// Derive key agreement on expanding did:key address
+    pub enable_encryption_key_derivation: bool,
 }
 
 impl DIDMethod for DIDKeyMethod {
@@ -59,11 +63,113 @@ impl DIDKeyMethod {
 
         Ok(format!("did:key:{}", multibase_value))
     }
+
+    /// Expands did:key address into DID document
+    ///
+    /// See https://w3c-ccg.github.io/did-method-key/#create
+    pub fn expand(&self, did: &str) -> Result<DIDDocument, DIDResolutionError> {
+        if !did.starts_with("did:key:") {
+            return Err(DIDResolutionError::InvalidDID);
+        }
+
+        let multibase_value = did.strip_prefix("did:key:").unwrap();
+        let (base, multicodec) = multibase::decode(multibase_value).map_err(|_| DIDResolutionError::InvalidDID)?;
+
+        // Validate decoded multibase value
+        if base != Base58Btc || multicodec.len() < 2 {
+            return Err(DIDResolutionError::InvalidDID);
+        }
+
+        // Partition multicodec value
+        let multicodec_prefix: &[u8; 2] = &multicodec[..2].try_into().unwrap();
+        let raw_public_key_bytes = &multicodec[2..];
+
+        // Derive algorithm from multicodec prefix
+        let alg = Algorithm::from_muticodec_prefix(multicodec_prefix).ok_or(DIDResolutionError::InvalidDID)?;
+
+        // Run algorithm for signature verification method expansion
+        let signature_verification_method = self.derive_signature_verification_method(&alg, multibase_value, raw_public_key_bytes)?;
+
+        // Build DID document
+        let diddoc = DIDDocument {
+            context: Context::SetOfString(self.guess_context_property(&alg)),
+            id: did.to_string(),
+            controller: None,
+            also_known_as: None,
+            verification_method: Some(vec![signature_verification_method.clone()]),
+            authentication: Some(vec![didcore::Authentication::Reference(
+                signature_verification_method.id.clone(), //
+            )]),
+            assertion_method: Some(vec![didcore::AssertionMethod::Reference(
+                signature_verification_method.id.clone(), //
+            )]),
+            capability_delegation: Some(vec![didcore::CapabilityDelegation::Reference(
+                signature_verification_method.id.clone(), //
+            )]),
+            capability_invocation: Some(vec![didcore::CapabilityInvocation::Reference(
+                signature_verification_method.id.clone(), //
+            )]),
+            key_agreement: None,
+            service: None,
+            additional_properties: None,
+            proof: None,
+        };
+
+        Ok(diddoc)
+    }
+
+    fn guess_context_property(&self, alg: &Algorithm) -> Vec<String> {
+        let mut context = vec!["https://www.w3.org/ns/did/v1"];
+
+        match self.key_format {
+            PublicKeyFormat::Multikey => match alg {
+                Algorithm::Ed25519 => context.push("https://w3id.org/security/suites/ed25519-2020/v1"),
+                Algorithm::X25519 => context.push("https://w3id.org/security/suites/x25519-2020/v1"),
+                _ => (),
+            },
+            PublicKeyFormat::Jwk => context.push("https://w3id.org/security/suites/jws-2020/v1"),
+        }
+
+        context.iter().map(|x| x.to_string()).collect()
+    }
+
+    // See https://w3c-ccg.github.io/did-method-key/#signature-method-creation-algorithm
+    fn derive_signature_verification_method(
+        &self,
+        alg: &Algorithm,
+        multibase_value: &str,
+        raw_public_key_bytes: &[u8],
+    ) -> Result<VerificationMethod, DIDResolutionError> {
+        if let Some(required_length) = alg.public_key_length() {
+            if required_length != raw_public_key_bytes.len() {
+                return Err(DIDResolutionError::InvalidPublicKeyLength);
+            }
+        }
+
+        Ok(VerificationMethod {
+            id: format!("did:key:{multibase_value}#{multibase_value}"),
+            key_type: String::from(match self.key_format {
+                PublicKeyFormat::Multikey => match alg {
+                    Algorithm::Ed25519 => "Ed25519VerificationKey2020",
+                    Algorithm::X25519 => "X25519KeyAgreementKey2020",
+                    _ => "Multikey",
+                },
+                PublicKeyFormat::Jwk => "JsonWebKey2020",
+            }),
+            controller: format!("did:key:{multibase_value}"),
+            public_key: Some(match self.key_format {
+                PublicKeyFormat::Multikey => KeyFormat::Multibase(String::from(multibase_value)),
+                PublicKeyFormat::Jwk => KeyFormat::Jwk(alg.build_jwk(raw_public_key_bytes)),
+            }),
+            ..VerificationMethod::default()
+        })
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serde_json::Value;
     use crate::didcore::Jwk;
 
     #[test]
@@ -118,5 +224,94 @@ mod tests {
             let did = DIDKeyMethod::from_raw_public_key(alg, &bytes);
             assert_eq!(did.unwrap(), expected);
         }
+    }
+
+    #[test]
+    fn test_did_key_expansion_multikey() {
+        let did_method = DIDKeyMethod::default();
+
+        let did = "did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK";
+        let expected: Value = serde_json::from_str(
+            r#"{
+                "@context": [
+                    "https://www.w3.org/ns/did/v1",
+                    "https://w3id.org/security/suites/ed25519-2020/v1"
+                ],
+                "id": "did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK",
+                "verificationMethod": [{
+                    "id": "did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK#z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK",
+                    "type": "Ed25519VerificationKey2020",
+                    "controller": "did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK",
+                    "publicKeyMultibase": "z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK"
+                }],
+                "authentication": ["did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK#z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK"],
+                "assertionMethod": ["did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK#z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK"],
+                "capabilityDelegation": ["did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK#z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK"],
+                "capabilityInvocation": ["did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK#z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK"]
+            }"#,
+        )
+        .unwrap();
+
+        let diddoc = did_method.expand(did).unwrap();
+
+        assert_eq!(
+            json_canon::to_string(&diddoc).unwrap(),   //
+            json_canon::to_string(&expected).unwrap(), //
+        );
+    }
+
+    #[test]
+    fn test_did_key_expansion_jsonwebkey() {
+        let did_method = DIDKeyMethod {
+            key_format: PublicKeyFormat::Jwk,
+            ..Default::default()
+        };
+
+        let did = "did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK";
+        let expected: Value = serde_json::from_str(
+            r#"{
+                "@context": [
+                    "https://www.w3.org/ns/did/v1",
+                    "https://w3id.org/security/suites/jws-2020/v1"
+                ],
+                "id": "did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK",
+                "verificationMethod": [{
+                    "id": "did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK#z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK",
+                    "type": "JsonWebKey2020",
+                    "controller": "did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK",
+                    "publicKeyJwk": {
+                        "kty": "OKP",
+                        "crv": "Ed25519",
+                        "x": "Lm_M42cB3HkUiODQsXRcweM6TByfzEHGO9ND274JcOY"
+                    }
+                }],
+                "authentication": ["did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK#z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK"],
+                "assertionMethod": ["did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK#z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK"],
+                "capabilityDelegation": ["did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK#z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK"],
+                "capabilityInvocation": ["did:key:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK#z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK"]
+            }"#,
+        )
+        .unwrap();
+
+        let diddoc = did_method.expand(did).unwrap();
+
+        assert_eq!(
+            json_canon::to_string(&diddoc).unwrap(),   //
+            json_canon::to_string(&expected).unwrap(), //
+        );
+    }
+
+    #[test]
+    fn test_did_key_expansion_fails_as_expected() {
+        let did_method = DIDKeyMethod::default();
+
+        let did = "did:key:Z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK";
+        assert_eq!(did_method.expand(did).unwrap_err(), DIDResolutionError::InvalidDID);
+
+        let did = "did:key:z6MkhaXgBZDvotDkL5257####tiGiC2QtKLGpbnnEGta2doK";
+        assert_eq!(did_method.expand(did).unwrap_err(), DIDResolutionError::InvalidDID);
+
+        let did = "did:key:zQebt6zPwbE4Vw5GFAjjARHrNXFALofERVv4q6Z4db8cnDRQm";
+        assert_eq!(did_method.expand(did).unwrap_err(), DIDResolutionError::InvalidPublicKeyLength);
     }
 }
