bf91e3a17fb4e7b2a27ac4df40c504f0a4a43bcb
Hugo Ibanez <hugo.ibanez@adorsys.com>
2023-09-25 17:47:47 +0200

    Added did web http connectors and template for mock server based on new DID key structure

---
+++ did-utils/src/methods/did_web/resolver.rs
@@ -0,0 +1,112 @@
+use hyper::{
+    client::{connect::Connect, HttpConnector},
+    http::uri::Scheme,
+    service::{make_service_fn, service_fn},
+    Body, Client, Request, Response, Server,
+};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+use hyper_tls::HttpsConnector;
+
+const DID_DOCUMENT: &'static str = r##"
+{
+  "@context": [
+    "https://www.w3.org/ns/did/v1",
+    "https://w3id.org/security/suites/jws-2020/v1"
+  ],
+  "id": "did:web:example.com",
+  "verificationMethod": [
+    {
+      "id": "did:web:example.com#key-0",
+      "type": "JsonWebKey2020",
+      "controller": "did:web:example.com",
+      "publicKeyJwk": {
+        "kty": "OKP",
+        "crv": "Ed25519",
+        "x": "0-e2i2_Ua1S5HbTYnVB0lj2Z2ytXu2-tYmDFf8f5NjU"
+      }
+    },
+    {
+      "id": "did:web:example.com#key-1",
+      "type": "JsonWebKey2020",
+      "controller": "did:web:example.com",
+      "publicKeyJwk": {
+        "kty": "OKP",
+        "crv": "X25519",
+        "x": "9GXjPGGvmRq9F6Ng5dQQ_s31mfhxrcNZxRGONrmH30k"
+      }
+    },
+    {
+      "id": "did:web:example.com#key-2",
+      "type": "JsonWebKey2020",
+      "controller": "did:web:example.com",
+      "publicKeyJwk": {
+        "kty": "EC",
+        "crv": "P-256",
+        "x": "38M1FDts7Oea7urmseiugGW7tWc3mLpJh6rKe7xINZ8",
+        "y": "nDQW6XZ7b_u2Sy9slofYLlG03sOEoug3I0aAPQ0exs4"
+      }
+    }
+  ],
+  "authentication": [
+    "did:web:example.com#key-0",
+    "did:web:example.com#key-2"
+  ],
+  "assertionMethod": [
+    "did:web:example.com#key-0",
+    "did:web:example.com#key-2"
+  ],
+  "keyAgreement": [
+    "did:web:example.com#key-1",
+    "did:web:example.com#key-2"
+  ]
+}"##;
+
+pub struct DidWebResolver<C>
+where
+    C: Connect + Send + Sync + Clone + 'static,
+{
+    client: Client<C>,
+    scheme: Scheme,
+}
+
+impl DidWebResolver<HttpConnector> {
+    pub fn http() -> DidWebResolver<HttpConnector> {
+        DidWebResolver {
+            client: Client::builder().build::<_, Body>(HttpConnector::new()),
+            scheme: Scheme::HTTP,
+        }
+    }
+}
+
+impl DidWebResolver<HttpsConnector<HttpConnector>> {
+    pub fn https() -> DidWebResolver<HttpsConnector<HttpConnector>> {
+        DidWebResolver {
+            client: Client::builder().build::<_, Body>(HttpsConnector::new()),
+            scheme: Scheme::HTTPS,
+        }
+    }
+}
+
+async fn mock_server_handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let response = match req.uri().path() {
+        "/.well-known/did.json" => Response::new(Body::from(DID_DOCUMENT)),
+        _ => Response::builder().status(404).body(Body::from("Not Found")).unwrap(),
+    };
+
+    Ok(response)
+}
+
+async fn create_mock_server(port: u16) -> String {
+    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(mock_server_handler)) });
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    let server = Server::bind(&addr).serve(make_svc);
+
+    tokio::spawn(async move {
+        server.await.unwrap();
+    });
+
+    "localhost".to_string()
+}
--- did-utils/src/methods/mod.rs
+++ did-utils/src/methods/mod.rs
@@ -2,5 +2,6 @@ pub mod errors;
 pub mod traits;
 
 pub mod did_key;
+pub mod did_web;
 
 mod utils;
