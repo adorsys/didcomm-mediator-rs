fd728264a299a702a3239da6925caac9184c4dbd
Hugo Ibanez <hugo.ibanez@adorsys.com>
2023-10-17 16:36:19 +0200

    completed key support

--- did-utils/src/key/key.rs
+++ did-utils/src/key/key.rs
@@ -1,7 +1,6 @@
 use serde::{ Deserialize, Serialize };
 
-use crate::key::ec::Ec;
-use crate::key::okp::Okp;
+use crate::key::{ ec::Ec, okp::Okp, rsa::Rsa, oct::Oct };
 
 /// A key type that can be contained in a JWK.
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
@@ -11,6 +10,13 @@ pub enum Key {
     /// An elliptic-curve key.
     Ec(Ec),
 
+    /// An RSA key.
+    Rsa(Rsa),
+
+    /// A symmetric key.
+    #[serde(rename = "oct")]
+    Oct(Oct),
+
     /// A CFRG-curve key.
     Okp(Okp),
 }
@@ -19,6 +25,20 @@ impl From<Ec> for Key {
     #[inline(always)]
     fn from(key: Ec) -> Self {
         Self::Ec(key)
+    }
+}
+
+impl From<Rsa> for Key {
+    #[inline(always)]
+    fn from(key: Rsa) -> Self {
+        Self::Rsa(key)
+    }
+}
+
+impl From<Oct> for Key {
+    #[inline(always)]
+    fn from(key: Oct) -> Self {
+        Self::Oct(key)
     }
 }
 
--- did-utils/src/key/mod.rs
+++ did-utils/src/key/mod.rs
@@ -3,5 +3,7 @@ pub mod bytes;
 pub mod key;
 pub mod secret;
 pub mod okp;
+pub mod rsa;
+pub mod oct;
 
 pub use bytes::Bytes;
