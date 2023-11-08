213383d1252483801ba5df9d8a1563f03280463c
Ingrid Kamga <ingrid.kamga@clemios.com>
2023-11-08 12:05:37 +0100

    Improve test structure

--- mediator-coordination/src/jose/jws.rs
+++ mediator-coordination/src/jose/jws.rs
@@ -19,8 +19,8 @@ pub enum JwsError {
     InvalidSignature,
     #[error("invalid signing key")]
     InvalidSigningKey,
-    #[error("missing private signing key")]
-    MissingSigningPrivateKey,
+    #[error("missing private key")]
+    MissingPrivateKey,
     #[error("signing error")]
     SigningError,
     #[error("unsupported algorithm")]
@@ -57,6 +57,7 @@ pub struct JwsHeader {
 }
 
 /// Issues a JSON Web Signature (JWS)
+#[allow(unused)]
 pub fn make_compact_jws(header: &JwsHeader, payload: Value, jwk: &Jwk) -> Result<String, JwsError> {
     let make_phrase = || -> Result<String, JwsError> {
         let encoded_header = {
@@ -93,23 +94,24 @@ pub fn make_compact_jws_ed25519(phrase: String, jwk: &Jwk) -> Result<String, Jws
         .try_into()
         .map_err(|_| JwsError::InvalidSigningKey)?;
     if keypair.secret_key.is_none() {
-        return Err(JwsError::MissingSigningPrivateKey);
+        return Err(JwsError::MissingPrivateKey);
     }
 
     let signature = keypair
         .sign(phrase.as_bytes())
         .map_err(|_| JwsError::SigningError)?;
-    let encoded_signature = Base64Url.encode(&signature);
+    let encoded_signature = Base64Url.encode(signature);
 
     Ok(format!("{phrase}.{encoded_signature}"))
 }
 
 /// Verifies a JSON Web Signature (JWS)
+#[allow(unused)]
 pub fn verify_compact_jws(jws: &str, jwk: &Jwk) -> Result<(), JwsError> {
     let header_encoded = jws.split('.').next().ok_or(JwsError::InvalidFormat)?;
     let header_decoded = String::from_utf8(
         Base64Url
-            .decode(&header_encoded)
+            .decode(header_encoded)
             .map_err(|_| JwsError::InvalidFormat)?,
     )
     .map_err(|_| JwsError::InvalidFormat)?;
@@ -147,9 +149,21 @@ fn verify_compact_jws_ed25519(jws: &str, jwk: &Jwk) -> Result<(), JwsError> {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     use serde_json::json;
 
-    use super::*;
+    use crate::util;
+
+    fn setup() -> Jwk {
+        let storage_dirpath = util::dotenv_flow_read("STORAGE_DIRPATH").unwrap();
+
+        let diddoc = util::read_diddoc(&storage_dirpath).unwrap();
+        let (_, pubkey) = util::extract_assertion_key(&diddoc).unwrap();
+
+        let keystore = util::read_keystore(&storage_dirpath).unwrap();
+        keystore.find_keypair(&pubkey).unwrap()
+    }
 
     #[test]
     fn can_serde_jws_header() {
@@ -177,6 +191,8 @@ mod tests {
 
     #[test]
     fn can_make_compact_jws() {
+        let jwk = setup();
+
         let header = JwsHeader {
             typ: Some(String::from("application/json")),
             kid: Some(String::from("did:web:mediators-r-us.com#keys-2")),
@@ -187,8 +203,6 @@ mod tests {
         let payload = json!({
             "content": "e1200a6c-d9a2-49b4-baa6-da86d643ce3c"
         });
-
-        let jwk = signing_jwk();
 
         let jws = make_compact_jws(&header, payload, &jwk).unwrap();
         let expected_jws = concat!(
@@ -203,6 +217,8 @@ mod tests {
 
     #[test]
     fn can_make_compact_jws_v2() {
+        let jwk = setup();
+
         let header = JwsHeader {
             alg: JwsAlg::EdDSA,
             ..Default::default()
@@ -211,8 +227,6 @@ mod tests {
         let payload = json!({
             "content": "Edwards, what have you done?"
         });
-
-        let jwk = signing_jwk();
 
         let jws = make_compact_jws(&header, payload, &jwk).unwrap();
         let expected_jws = concat!(
@@ -226,6 +240,8 @@ mod tests {
 
     #[test]
     fn can_verify_compact_jws() {
+        let jwk = Jwk { d: None, ..setup() };
+
         let entries = [
             concat!(
                 "eyJ0eXAiOiJhcHBsaWNhdGlvbi9qc29uIiwia2lkIjoiZGlkOndlYjptZWRpYXRvcnMtci11",
@@ -240,37 +256,8 @@ mod tests {
             ),
         ];
 
-        let jwk = verifying_jwk();
-
         for entry in entries {
             assert!(verify_compact_jws(entry, &jwk).is_ok());
         }
-    }
-
-    //-------------------------------------------------------------------------
-    // Helpers ----------------------------------------------------------------
-    //-------------------------------------------------------------------------
-
-    fn signing_jwk() -> Jwk {
-        serde_json::from_str(
-            r#"{
-                "kty": "OKP",
-                "crv": "Ed25519",
-                "x": "Z0GqpN71rMcnAkky6_J6Bfknr8B-TBsekG3qdI0EQX4",
-                "d": "fI1u4riKKd99eox08GlThknq-vEJXcKBI28aiUqArLo"
-            }"#,
-        )
-        .unwrap()
-    }
-
-    fn verifying_jwk() -> Jwk {
-        serde_json::from_str(
-            r#"{
-                "kty": "OKP",
-                "crv": "Ed25519",
-                "x": "Z0GqpN71rMcnAkky6_J6Bfknr8B-TBsekG3qdI0EQX4"
-            }"#,
-        )
-        .unwrap()
     }
 }
