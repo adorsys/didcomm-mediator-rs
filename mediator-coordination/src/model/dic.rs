953593622ac70288cdb84f5cd7ff78ec6009d5c9
Ingrid Kamga <ingrid.kamga@clemios.com>
2023-11-08 12:50:54 +0100

    Implement methods for issuing JWTs with (D)DIC payloads

--- mediator-coordination/src/model/dic.rs
+++ mediator-coordination/src/model/dic.rs
@@ -1,6 +1,9 @@
+use did_utils::didcore::Jwk;
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
+use serde_json::{json, Value};
 use std::collections::HashMap;
+
+use crate::jose::jws::{self, JwsAlg, JwsError, JwsHeader};
 
 /// A DIC's service level discriminates the operability of the channel.
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Default)]
@@ -62,9 +65,55 @@ pub struct DDICPayload {
     pub additional_properties: Option<HashMap<String, Value>>,
 }
 
+#[allow(unused)]
+impl DICPayload {
+    const TYP: &'static str = "dic/v001";
+
+    /// Sign into JWT
+    fn sign(&self, jwk: &Jwk, kid: Option<String>) -> Result<String, JwsError> {
+        let header = JwsHeader {
+            typ: Some(Self::TYP.to_string()),
+            kid,
+            alg: JwsAlg::EdDSA,
+            ..Default::default()
+        };
+
+        jws::make_compact_jws(&header, json!(self), jwk)
+    }
+}
+
+#[allow(unused)]
+impl DDICPayload {
+    const TYP: &'static str = "ddic/v001";
+
+    /// Sign into JWT
+    fn sign(&self, jwk: &Jwk, kid: Option<String>) -> Result<String, JwsError> {
+        let header = JwsHeader {
+            typ: Some(Self::TYP.to_string()),
+            kid,
+            alg: JwsAlg::EdDSA,
+            ..Default::default()
+        };
+
+        jws::make_compact_jws(&header, json!(self), jwk)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    use crate::util;
+
+    fn setup() -> Jwk {
+        let storage_dirpath = util::dotenv_flow_read("STORAGE_DIRPATH").unwrap();
+
+        let diddoc = util::read_diddoc(&storage_dirpath).unwrap();
+        let (_, pubkey) = util::extract_assertion_key(&diddoc).unwrap();
+
+        let keystore = util::read_keystore(&storage_dirpath).unwrap();
+        keystore.find_keypair(&pubkey).unwrap()
+    }
 
     #[test]
     fn can_serde_dic_service_level() {
@@ -148,5 +197,66 @@ mod tests {
             json_canon::to_string(&ddic_payload).unwrap(),
             json_canon::to_string(&serde_json::from_str::<Value>(msg).unwrap()).unwrap(),
         )
+    }
+
+    #[test]
+    fn can_sign_dic_payload_into_jwt() {
+        let jwk = setup();
+
+        let dic_payload: DICPayload = serde_json::from_str(
+            r#"{
+                "sub": "did:key:alice_identity_pub@alice_mediator",
+                "iss": "did:web:alice-mediator.com:alice_mediator_pub",
+                "sl": "gold",
+                "nonce": "43f84868-0632-4471-b6dd-d63fa12c21f6"
+            }"#,
+        )
+        .unwrap();
+
+        // Sign
+        let jwt = dic_payload.sign(&jwk, None).unwrap();
+        let expected_jwt = concat!(
+            "eyJ0eXAiOiJkaWMvdjAwMSIsImFsZyI6IkVkRFNBIn0.eyJpc3MiOiJkaWQ6d2ViOmFsaWNl",
+            "LW1lZGlhdG9yLmNvbTphbGljZV9tZWRpYXRvcl9wdWIiLCJub25jZSI6IjQzZjg0ODY4LTA2",
+            "MzItNDQ3MS1iNmRkLWQ2M2ZhMTJjMjFmNiIsInNsIjoiZ29sZCIsInN1YiI6ImRpZDprZXk6",
+            "YWxpY2VfaWRlbnRpdHlfcHViQGFsaWNlX21lZGlhdG9yIn0.tslxNKmgVX_LhKIM5SH9KIxp",
+            "_jCAXGNmjuisS2SmmGlXf2LuR3iUeAPXWm9f0XA1_jvVXw7gJLlbJFer6zSCDA"
+        );
+        assert_eq!(jwt, expected_jwt);
+
+        // Verify
+        let jwk = Jwk { d: None, ..jwk };
+        assert!(jws::verify_compact_jws(&jwt, &jwk).is_ok());
+    }
+
+    #[test]
+    fn can_sign_ddic_payload_into_jwt() {
+        let jwk = setup();
+
+        let ddic_payload: DDICPayload = serde_json::from_str(
+            r#"{
+                "sub": "did:key:bob_identity_pub@alice",
+                "iss": "did:web:alice-mediator.com:alice_mediator_pub",
+                "dic-sub": "did:key:alice_identity_pub@alice_mediator",
+                "nonce": "43f84868-0632-4471-b6dd-d63fa12c21f6"
+            }"#,
+        )
+        .unwrap();
+
+        // Sign
+        let jwt = ddic_payload.sign(&jwk, None).unwrap();
+        let expected_jwt = concat!(
+            "eyJ0eXAiOiJkZGljL3YwMDEiLCJhbGciOiJFZERTQSJ9.eyJkaWMtc3ViIjoiZGlkOmtleTp",
+            "hbGljZV9pZGVudGl0eV9wdWJAYWxpY2VfbWVkaWF0b3IiLCJpc3MiOiJkaWQ6d2ViOmFsaWN",
+            "lLW1lZGlhdG9yLmNvbTphbGljZV9tZWRpYXRvcl9wdWIiLCJub25jZSI6IjQzZjg0ODY4LTA",
+            "2MzItNDQ3MS1iNmRkLWQ2M2ZhMTJjMjFmNiIsInN1YiI6ImRpZDprZXk6Ym9iX2lkZW50aXR",
+            "5X3B1YkBhbGljZSJ9.TMrKBQ22yCY-A07bIaR6c73Y9LK-rorKv9wvoh1NnYGgr2IzIvMP8g",
+            "NjQmizpgjdyVXz8KlXr8F_ARl_iQ-MDA"
+        );
+        assert_eq!(jwt, expected_jwt);
+
+        // Verify
+        let jwk = Jwk { d: None, ..jwk };
+        assert!(jws::verify_compact_jws(&jwt, &jwk).is_ok());
     }
 }
