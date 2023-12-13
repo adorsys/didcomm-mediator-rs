03d43b28c77f05a2c18ea91937ba5bbc26e3512d
Ingrid Kamga <ingrid.kamga@clemios.com>
2023-12-13 15:41:00 +0100

    Align tests with updated structuration

--- mediator-coordination/src/model/stateful/coord.rs
+++ mediator-coordination/src/model/stateful/coord.rs
@@ -1,11 +1,23 @@
+//! Types for Coordinate Mediation v2.0
+//! See https://didcomm.org/coordinate-mediation/2.0
+
 use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
-/// Message to notify the mediator of keys in use by the recipient.
+/// Header for Transports Return Route Extension
 ///
-/// See https://github.com/hyperledger/aries-rfcs/tree/main/features/0211-route-coordination#keylist-update
+/// See https://github.com/hyperledger/aries-rfcs/tree/main/features/0092-transport-return-route
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+#[serde(rename_all = "snake_case")]
+pub enum ReturnRouteHeader {
+    None,
+    Thread,
+    All,
+}
+
+/// Message to notify the mediator of keys in use by the recipient.
 #[derive(Debug, Serialize, Deserialize, Clone, Default)]
 #[serde(rename_all = "snake_case")]
 pub struct KeylistUpdate {
@@ -19,20 +31,31 @@ pub struct KeylistUpdate {
     #[serde(rename = "@type")]
     pub message_type: String,
 
-    /// List of commands to update keys in use
-    pub updates: Vec<KeylistUpdateCommand>,
+    /// Message body
+    pub body: KeylistUpdateBody,
+
+    /// Return route header
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub return_route: Option<ReturnRouteHeader>,
 
     /// Dynamic properties.
     #[serde(flatten)]
     pub additional_properties: Option<HashMap<String, Value>>,
 }
 
+#[derive(Debug, Serialize, Deserialize, Clone, Default, PartialEq, Eq)]
+#[serde(rename_all = "snake_case")]
+pub struct KeylistUpdateBody {
+    /// List of commands to update keys in use
+    pub updates: Vec<KeylistUpdateCommand>,
+}
+
 /// Specifies a command for keylist update
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
 #[serde(rename_all = "snake_case")]
 pub struct KeylistUpdateCommand {
-    /// Key on which to apply an action
-    pub recipient_key: String,
+    /// DID on which to apply an action
+    pub recipient_did: String,
 
     /// Add or remove
     pub action: KeylistUpdateAction,
@@ -47,8 +70,6 @@ pub enum KeylistUpdateAction {
 }
 
 /// Response message to confirm requested keylist updates.
-///
-/// See https://github.com/hyperledger/aries-rfcs/tree/main/features/0211-route-coordination#keylist-update-response
 #[derive(Debug, Serialize, Deserialize, Clone, Default)]
 #[serde(rename_all = "snake_case")]
 pub struct KeylistUpdateResponse {
@@ -62,20 +83,27 @@ pub struct KeylistUpdateResponse {
     #[serde(rename = "@type")]
     pub message_type: String,
 
-    /// Confirmations to requested keylist updates.
-    pub updated: Vec<KeylistUpdateConfirmation>,
+    /// Message body
+    pub body: KeylistUpdateResponseBody,
 
     /// Dynamic properties.
     #[serde(flatten)]
     pub additional_properties: Option<HashMap<String, Value>>,
 }
 
+#[derive(Debug, Serialize, Deserialize, Clone, Default, PartialEq, Eq)]
+#[serde(rename_all = "snake_case")]
+pub struct KeylistUpdateResponseBody {
+    /// Confirmations to requested keylist updates.
+    pub updated: Vec<KeylistUpdateConfirmation>,
+}
+
 /// Conveys a result to a requested keylist update.
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
 #[serde(rename_all = "snake_case")]
 pub struct KeylistUpdateConfirmation {
-    /// Key at which an action is directed
-    pub recipient_key: String,
+    /// DID at which an action was directed
+    pub recipient_did: String,
 
     /// Add or remove
     pub action: KeylistUpdateAction,
@@ -95,8 +123,6 @@ pub enum KeylistUpdateResult {
 }
 
 /// Message to query mediator for a list of keys registered for this connection.
-///
-/// See https://github.com/hyperledger/aries-rfcs/tree/main/features/0211-route-coordination#key-list-query
 #[derive(Debug, Serialize, Deserialize, Clone, Default)]
 #[serde(rename_all = "snake_case")]
 pub struct KeylistQuery {
@@ -110,12 +136,24 @@ pub struct KeylistQuery {
     #[serde(rename = "@type")]
     pub message_type: String,
 
-    /// Optional pagination details.
-    pub paginate: Option<KeylistQueryPaginate>,
+    /// Message body
+    pub body: KeylistQueryBody,
+
+    /// Return route header
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub return_route: Option<ReturnRouteHeader>,
 
     /// Dynamic properties.
     #[serde(flatten)]
     pub additional_properties: Option<HashMap<String, Value>>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Default, PartialEq, Eq)]
+#[serde(rename_all = "snake_case")]
+pub struct KeylistQueryBody {
+    /// Optional pagination details.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub paginate: Option<KeylistQueryPaginate>,
 }
 
 /// Pagination details for a keylist query.
@@ -127,8 +165,6 @@ pub struct KeylistQueryPaginate {
 }
 
 /// Response to key list query, containing retrieved keys.
-///
-/// See https://github.com/hyperledger/aries-rfcs/tree/main/features/0211-route-coordination#key-list
 #[derive(Debug, Serialize, Deserialize, Clone, Default)]
 #[serde(rename_all = "snake_case")]
 pub struct Keylist {
@@ -142,23 +178,31 @@ pub struct Keylist {
     #[serde(rename = "@type")]
     pub message_type: String,
 
-    /// List of retrieved keys.
-    pub keys: Vec<KeylistEntry>,
-
-    /// Optional pagination details.
-    pub pagination: Option<KeylistPagination>,
+    /// Message body
+    pub body: KeylistBody,
 
     /// Dynamic properties.
     #[serde(flatten)]
     pub additional_properties: Option<HashMap<String, Value>>,
 }
 
+#[derive(Debug, Serialize, Deserialize, Clone, Default, PartialEq, Eq)]
+#[serde(rename_all = "snake_case")]
+pub struct KeylistBody {
+    /// List of retrieved keys.
+    pub keys: Vec<KeylistEntry>,
+
+    /// Optional pagination details.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub pagination: Option<KeylistPagination>,
+}
+
 /// Keylist entry for a specific key.
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
 #[serde(rename_all = "snake_case")]
 pub struct KeylistEntry {
-    /// Retrieved key
-    pub recipient_key: String,
+    /// Registered DID
+    pub recipient_did: String,
 }
 
 /// Pagination details for a keylist query.
@@ -179,6 +223,26 @@ mod tests {
     use crate::constant::*;
 
     #[test]
+    fn can_serde_return_route_header_enum() {
+        let variants = [
+            (ReturnRouteHeader::None, r#""none""#),
+            (ReturnRouteHeader::Thread, r#""thread""#),
+            (ReturnRouteHeader::All, r#""all""#),
+        ];
+
+        for (variant, serialized) in variants {
+            // Serialization
+            assert_eq!(serde_json::to_string(&variant).unwrap(), serialized);
+
+            // De-serialization
+            assert_eq!(
+                serde_json::from_str::<ReturnRouteHeader>(serialized).unwrap(),
+                variant
+            );
+        }
+    }
+
+    #[test]
     fn can_serde_keylist_update_action_enum() {
         let variants = [
             (KeylistUpdateAction::Add, r#""add""#),
@@ -195,71 +259,6 @@ mod tests {
                 variant
             );
         }
-    }
-
-    #[test]
-    fn can_serialize_keylist_update_message() {
-        let keylist_update = KeylistUpdate {
-            id: "id_alice_keylist_update_request".to_string(),
-            message_type: KEYLIST_UPDATE_2_0.to_string(),
-            updates: vec![KeylistUpdateCommand {
-                recipient_key: String::from("did:key:alice_identity_pub1@alice_mediator"),
-                action: KeylistUpdateAction::Add,
-            }],
-            additional_properties: None,
-        };
-
-        let expected = json!({
-            "@id": "id_alice_keylist_update_request",
-            "@type": "https://didcomm.org/coordinate-mediation/2.0/keylist-update",
-            "updates": [
-                {
-                    "recipient_key": "did:key:alice_identity_pub1@alice_mediator",
-                    "action": "add",
-                }
-            ]
-        });
-
-        assert_eq!(
-            json_canon::to_string(&keylist_update).unwrap(),
-            json_canon::to_string(&expected).unwrap(),
-        )
-    }
-
-    #[test]
-    fn can_deserialize_keylist_update_message() {
-        let msg = r#"{
-            "@id": "id_alice_keylist_update_request",
-            "@type": "https://didcomm.org/coordinate-mediation/2.0/keylist-update",
-            "updates": [
-                {
-                    "recipient_key": "did:key:alice_identity_pub1@alice_mediator",
-                    "action": "add"
-                }
-            ]
-        }"#;
-
-        // Assert deserialization
-
-        let keylist_update: KeylistUpdate = serde_json::from_str(msg).unwrap();
-
-        assert_eq!(&keylist_update.id, "id_alice_keylist_update_request");
-        assert_eq!(&keylist_update.message_type, KEYLIST_UPDATE_2_0);
-
-        assert_eq!(
-            keylist_update.updates,
-            vec![KeylistUpdateCommand {
-                recipient_key: String::from("did:key:alice_identity_pub1@alice_mediator"),
-                action: KeylistUpdateAction::Add,
-            }]
-        );
-
-        // Assert re-serialization
-
-        assert_eq!(
-            json_canon::to_string(&keylist_update).unwrap(),
-            json_canon::to_string(&serde_json::from_str::<Value>(msg).unwrap()).unwrap(),
-        )
     }
 
     #[test]
@@ -284,28 +283,108 @@ mod tests {
     }
 
     #[test]
+    fn can_serialize_keylist_update_message() {
+        let keylist_update = KeylistUpdate {
+            id: "id_alice_keylist_update_request".to_string(),
+            message_type: KEYLIST_UPDATE_2_0.to_string(),
+            body: KeylistUpdateBody {
+                updates: vec![KeylistUpdateCommand {
+                    recipient_did: String::from("did:key:alice_identity_pub1@alice_mediator"),
+                    action: KeylistUpdateAction::Add,
+                }],
+            },
+            return_route: Some(ReturnRouteHeader::All),
+            additional_properties: None,
+        };
+
+        let expected = json!({
+            "@id": "id_alice_keylist_update_request",
+            "@type": "https://didcomm.org/coordinate-mediation/2.0/keylist-update",
+            "body": {
+                "updates": [
+                    {
+                        "recipient_did": "did:key:alice_identity_pub1@alice_mediator",
+                        "action": "add",
+                    }
+                ]
+            },
+            "return_route": "all",
+        });
+
+        assert_eq!(
+            json_canon::to_string(&keylist_update).unwrap(),
+            json_canon::to_string(&expected).unwrap(),
+        )
+    }
+
+    #[test]
+    fn can_deserialize_keylist_update_message() {
+        let msg = r#"{
+            "@id": "id_alice_keylist_update_request",
+            "@type": "https://didcomm.org/coordinate-mediation/2.0/keylist-update",
+            "body": {
+                "updates": [
+                    {
+                        "recipient_did": "did:key:alice_identity_pub1@alice_mediator",
+                        "action": "add"
+                    }
+                ]
+            },
+            "return_route": "all"
+        }"#;
+
+        // Assert deserialization
+
+        let keylist_update: KeylistUpdate = serde_json::from_str(msg).unwrap();
+
+        assert_eq!(&keylist_update.id, "id_alice_keylist_update_request");
+        assert_eq!(&keylist_update.message_type, KEYLIST_UPDATE_2_0);
+        assert_eq!(keylist_update.return_route, Some(ReturnRouteHeader::All));
+
+        assert_eq!(
+            keylist_update.body,
+            KeylistUpdateBody {
+                updates: vec![KeylistUpdateCommand {
+                    recipient_did: String::from("did:key:alice_identity_pub1@alice_mediator"),
+                    action: KeylistUpdateAction::Add,
+                }]
+            }
+        );
+
+        // Assert re-serialization
+
+        assert_eq!(
+            json_canon::to_string(&keylist_update).unwrap(),
+            json_canon::to_string(&serde_json::from_str::<Value>(msg).unwrap()).unwrap(),
+        )
+    }
+
+    #[test]
     fn can_serialize_keylist_update_response_message() {
         let keylist_update_response = KeylistUpdateResponse {
             id: "id_alice_keylist_update_response".to_string(),
             message_type: KEYLIST_UPDATE_RESPONSE_2_0.to_string(),
-            updated: vec![KeylistUpdateConfirmation {
-                recipient_key: String::from("did:key:alice_identity_pub1@alice_mediator"),
-                action: KeylistUpdateAction::Add,
-                result: KeylistUpdateResult::Success,
-            }],
+            body: KeylistUpdateResponseBody {
+                updated: vec![KeylistUpdateConfirmation {
+                    recipient_did: String::from("did:key:alice_identity_pub1@alice_mediator"),
+                    action: KeylistUpdateAction::Add,
+                    result: KeylistUpdateResult::Success,
+                }],
+            },
             additional_properties: None,
         };
 
         let expected = json!({
             "@id": "id_alice_keylist_update_response",
             "@type": "https://didcomm.org/coordinate-mediation/2.0/keylist-update-response",
+            "body": {
             "updated": [
                 {
-                    "recipient_key": "did:key:alice_identity_pub1@alice_mediator",
+                    "recipient_did": "did:key:alice_identity_pub1@alice_mediator",
                     "action": "add",
                     "result": "success",
                 }
-            ]
+            ]}
         });
 
         assert_eq!(
@@ -319,13 +398,15 @@ mod tests {
         let msg = r#"{
             "@id": "id_alice_keylist_update_response",
             "@type": "https://didcomm.org/coordinate-mediation/2.0/keylist-update-response",
-            "updated": [
-                {
-                    "recipient_key": "did:key:alice_identity_pub1@alice_mediator",
-                    "action": "add",
-                    "result": "success"
-                }
-            ]
+            "body": {
+                "updated": [
+                    {
+                        "recipient_did": "did:key:alice_identity_pub1@alice_mediator",
+                        "action": "add",
+                        "result": "success"
+                    }
+                ]
+            }
         }"#;
 
         // Assert deserialization
@@ -342,12 +423,14 @@ mod tests {
         );
 
         assert_eq!(
-            keylist_update_response.updated,
-            vec![KeylistUpdateConfirmation {
-                recipient_key: String::from("did:key:alice_identity_pub1@alice_mediator"),
-                action: KeylistUpdateAction::Add,
-                result: KeylistUpdateResult::Success,
-            }]
+            keylist_update_response.body,
+            KeylistUpdateResponseBody {
+                updated: vec![KeylistUpdateConfirmation {
+                    recipient_did: String::from("did:key:alice_identity_pub1@alice_mediator"),
+                    action: KeylistUpdateAction::Add,
+                    result: KeylistUpdateResult::Success,
+                }]
+            }
         );
 
         // Assert re-serialization
