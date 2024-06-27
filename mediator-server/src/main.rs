723909e9f33d39fad6ed5085718ecf772c3c479d
Christiantyemele <yemelechristian2@gmail.com>
2024-06-27 17:42:40 +0100

    feat(server): gracefully shutting down mediator-server

--- mediator-server/src/main.rs
+++ mediator-server/src/main.rs
@@ -1,45 +1,74 @@
-use std::net::SocketAddr;
-
-use axum::{response::{IntoResponse, Response}, Router, middleware, Json, http::{Uri, Method}};
+use axum::{
+    http::{Method, Uri},
+    middleware,
+    response::{IntoResponse, Response},
+    Json, Router,
+};
 use serde_json::json;
+use std::net::SocketAddr;
+use tokio_util::task::TaskTracker;
 use uuid::Uuid;
 
+mod constants;
 mod coordinate_mediation;
 mod ctx;
-mod constants;
 mod error;
-mod models;
 mod log;
+mod models;
 mod utils;
 
-use crate::{models::RecipientController, log::log_request};
+use crate::{log::log_request, models::RecipientController};
 
-pub use self::{error::{Error, Result, ClientError}, ctx::Ctx};
+pub use self::{
+    ctx::Ctx,
+    error::{ClientError, Error, Result},
+};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let mc = RecipientController::new().await;
     let routes_mediate_request = coordinate_mediation::routes_mediate_request::routes(mc.clone());
     let routes_all = Router::new()
-        .nest("/coordinate-mediation/2.0/mediate-request", routes_mediate_request)
+        .nest(
+            "/coordinate-mediation/2.0/mediate-request",
+            routes_mediate_request,
+        )
         .layer(middleware::map_response(main_response_mapper));
 
+    // create a messager which will send the shutdown message to the server and its processes
+    // any process which wishes to stop the server can send a shutdown message to the shutdown transmitter
+    let (mut shutdown_tx, mut shutdown_rx) = tokio::sync::mpsc::channel::<String>(2);
+
+    // Create a tracker wish will use to track and wait for operations to finish
+    // before shutdown
+    let tracker = TaskTracker::new();
+
+    // first condition satisfied
+    tracker.close();
+    // shutdown_tx.send("value");
     // region: --- Start Server
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("->> Listening on {addr}\n");
-    axum::Server::bind(&addr)
-        .serve(routes_all.into_make_service())
-        .await
-        .unwrap();
+
+    // spawning task tracker on server to handle server's shutdown
+    tracker.spawn(async move {
+        axum::Server::bind(&addr)
+            .serve(routes_all.into_make_service())
+            .await
+            .unwrap();
+    });
     // endregion: --- Start Server
+
+    // gracyfully shuting down the server on CTRL-C or on shutdown alert from shutdown transmitter
+    // select on the operations for which we wish to gracefully shutdown the server
+    tokio::select! {
+            _shutdown_message = shutdown_rx.recv() => { print!("{}", _shutdown_message.unwrap()); tracker.wait().await;},
+            _ = tokio::signal::ctrl_c() => {eprintln!("shutting down")},
+    }
 
     Ok(())
 }
-
-async fn main_response_mapper(
-    uri: Uri,
-    req_method: Method,
-    res:Response) -> Response {
+async fn main_response_mapper(uri: Uri, req_method: Method, res: Response) -> Response {
     println!("->> {:<12} - main_response_mapper", "RESPONSE_MAPPER");
     let uuid = Uuid::new_v4();
 
@@ -61,9 +90,8 @@ async fn main_response_mapper(
 
             // Build the new response from the client_error_body.
             (*status_code, Json(client_error_body)).into_response()
-
         });
-    
+
     // -- Build and log the server log line
     let client_error = client_status_error.unzip().1;
     let _ = log_request(uuid, req_method, uri, service_error, client_error).await;
