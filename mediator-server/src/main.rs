47a5fb9062563c59f5566e9e65f621304f3a160c
Christiantyemele <yemelechristian2@gmail.com>
2024-07-04 16:45:20 +0100

    feat(): gracefull shutdown

--- mediator-server/src/main.rs
+++ mediator-server/src/main.rs
@@ -4,10 +4,9 @@ use axum::{
     response::{IntoResponse, Response},
     Json, Router,
 };
-use once_cell::sync::Lazy;
+
 use serde_json::json;
-use std::{net::SocketAddr, sync::Arc};
-use tokio::sync::{mpsc::Sender, Mutex};
+use std::net::SocketAddr;
 use tokio_util::sync::CancellationToken;
 use uuid::Uuid;
 
@@ -25,15 +24,13 @@ pub use self::{
     ctx::Ctx,
     error::{ClientError, Error, Result},
 };
-// create a global signal shutdown signal transmitter
-static SHUTDOWN: Lazy<Arc<Mutex<Option<Sender<String>>>>> = Lazy::new(|| Arc::new(Mutex::new(None)));
 
 #[tokio::main]
 async fn main() -> Result<()> {
     run_shutdown().await;
     Ok(())
 }
-    async fn run_shutdown() {
+async fn run_shutdown() {
     let mc = RecipientController::new().await;
     let routes_mediate_request = coordinate_mediation::routes_mediate_request::routes(mc.clone());
     let routes_all = Router::new()
@@ -43,20 +40,12 @@ async fn main() -> Result<()> {
         )
         .layer(middleware::map_response(main_response_mapper));
 
-    // create a messager which will send the shutdown message to the server and its processes
-    // any process which wishes to stop the server can send a shutdown message to the shutdown transmitter
-    let (shutdown_tx, mut shutdown_rx) = tokio::sync::mpsc::channel::<String>(2);
-
     // create cancellation tokens which when closed will tell processes to shutdown
     let token = CancellationToken::new();
 
     // region: --- Start Server
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("->> Listening on {addr}\n");
-
-// initialising global shutdown transmitter
- let mut lock = SHUTDOWN.lock().await;
- lock.replace(shutdown_tx);
 
     // spawning task on server to handle server's shutdown
     tokio::spawn(async move {
@@ -67,11 +56,9 @@ async fn main() -> Result<()> {
     });
     // endregion: --- Start Server
 
-    // gracyfully shutting down the server on CTRL-C or on shutdown alert from shutdown transmitter
     // select on the operations for which we wish to gracefully shutdown the server
     tokio::select! {
-            _shutdown_message = shutdown_rx.recv() => {eprintln!("shutting down"); token.cancel()},
-            _ = tokio::signal::ctrl_c() => {eprintln!("shutting down"); token.cancel()},
+            _ = tokio::signal::ctrl_c() => {eprintln!("shutting down"); token.cancel()}
     }
 }
 
@@ -110,23 +97,12 @@ async fn main_response_mapper(uri: Uri, req_method: Method, res: Response) -> Re
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    use tokio::signal;
     #[tokio::test]
     async fn test_server_shutdown_with_shutdown_signal() {
-      
         // run server in background
         tokio::spawn(run_shutdown());
-
-        // send shutdown signal
-        let mut lock = SHUTDOWN.lock().await;
-        let sender = lock.as_mut();
-        match sender {
-            Some(sender) => {
-                sender.send("Shutdown".to_owned()).await.unwrap();
-
-            }
-            None => {}
-        }
-
+        // send a shutdown signal to main thread
+        signal::unix::SignalKind::terminate();
     }
 }
