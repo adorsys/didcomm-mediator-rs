36cb9e803bc3f87819088487041c58cda1ec2c5a
Hermann Core <hermannoenpause@gmail.com>
2024-08-08 15:14:28 +0100

    remove ignored folder from the index

--- mediator-server/src/constants.rs
+++
@@ -1,7 +0,0 @@
-pub const MEDIATE_REQUEST_2_0: &str = "https://didcomm.org/coordinate-mediation/2.0/mediate-request";
-pub const MEDIATE_DENY_2_0: &str = "https://didcomm.org/coordinate-mediation/2.0/mediate-deny";
-pub const MEDIATE_GRANT_2_0: &str = "https://didcomm.org/coordinate-mediation/2.0/mediate-grant";
-pub const KEYLIST_UPDATE_2_0: &str = "https://didcomm.org/coordinate-mediation/2.0/keylist-update";
-pub const KEYLIST_UPDATE_RESPONSE_2_0: &str = "https://didcomm.org/coordinate-mediation/2.0/keylist-update-response";
-pub const KEYLIST_QUERY_2_0: &str = "https://didcomm.org/coordinate-mediation/2.0/keylist-query";
-pub const KEYLIST_2_0: &str = "https://didcomm.org/coordinate-mediation/2.0/keylist";
