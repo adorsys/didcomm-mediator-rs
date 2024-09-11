5ba28a7bc1dc00e9cf2a69e830ace009fe73685b
Hermann Core <hermannoenpause@gmail.com>
2024-09-11 15:46:29 +0100

    Define models for the status request pickup protocol

--- crates/plugins/message-pickup/src/model.rs
+++ crates/plugins/message-pickup/src/model.rs
@@ -1,23 +1,56 @@
-use serde_json::Value;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
-pub(crate) struct StatusRequest {
-    id: String,
+pub(crate) struct StatusRequest<'a> {
+    pub(crate) id: &'a str,
 
     #[serde(rename = "type")]
-    type_: String,
+    pub(crate) type_: &'a str,
 
-    body: BodyResponse,
+    pub(crate) body: BodyStatusRequest<'a>,
 
-    return_route: ReturnRoute,
+    pub(crate) return_route: ReturnRoute,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub(crate) struct BodyResponse {
+pub(crate) struct BodyStatusRequest<'a> {
     #[serde(skip_serializing_if = "Option::is_none")]
-    recipient_did: Option<String>,
+    recipient_did: Option<&'a str>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct StatusResponse<'a> {
+    pub(crate) id: &'a str,
+
+    #[serde(rename = "type")]
+    pub(crate) type_: &'a str,
+
+    pub(crate) body: BodyStatusResponse<'a>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub(crate) struct BodyStatusResponse<'a> {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) recipient_did: Option<&'a str>,
+
+    pub(crate) message_count: u32,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) longest_waited_seconds: Option<u32>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) lnewest_received_time: Option<u32>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) oldest_received_time: Option<u32>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) total_bytes: Option<u32>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) live_delivery: Option<bool>,
 }
 
 #[derive(Debug, Default, Serialize, Deserialize)]
