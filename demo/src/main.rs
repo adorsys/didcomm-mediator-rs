dec5d8dda3ec1cefdb3254b95184704588654229
ndefokou <tcheutchouaarthur38@gmail.com>
2024-09-19 12:15:35 +0100

    implimenting demo for get_mediator_didoc,GET THE KEYLIST UPDATE PAYLOAD,GET THE KEYLIST QUERY PAYLOAD

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,24 +1,41 @@
-fn main() {
-    println!("Hello, world!");
-}
-use std::collections::HashMap;
-
 use didcomm::{
     did::resolvers::ExampleDIDResolver, secrets::resolvers::ExampleSecretsResolver, Message,
     PackEncryptedOptions, UnpackOptions,
 };
 use ledger::{ALICE_DID, ALICE_DID_DOC, ALICE_SECRETS, MEDIATOR_DID, MEDIATOR_DID_DOC};
-use reqwest::header::CONTENT_TYPE;
+use reqwest::{header::CONTENT_TYPE, Client};
 use serde_json::json;
 const DIDCOMM_CONTENT_TYPE: &str = "application/didcomm-encrypted+json";
-static ROUTING_DID: Lazy<String> = Lazy::new(|| {});
 
 mod ledger;
 #[tokio::main]
 async fn main() {
+    println!("\n=================== GET THE DID DOCUMENT ===================\n");
+    get_mediator_didoc().await;
+
     println!("\n=================== MEDIATING REQUEST ===================\n");
     mediate_request().await;
+
+    println!("\n=================== GET THE KEYLIST UPDATE PAYLOAD ===================\n");
+    keylist_update_payload().await;
+
+    println!("\n=================== GET THE KEYLIST QUERY PAYLOAD ===================\n");
+    keylist_query_payload().await;
 }
+
+async fn get_mediator_didoc() {
+    let client = reqwest::Client::new();
+    let did_doc = client
+        .get("http://localhost:3000/.well-known/did.json")
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+    println!("{}", did_doc);
+}
+
 async fn mediate_request() {
     let did_resolver =
         ExampleDIDResolver::new(vec![ALICE_DID_DOC.clone(), MEDIATOR_DID_DOC.clone()]);
@@ -36,7 +53,6 @@ async fn mediate_request() {
     .finalize();
 
     // Encrypt message for mediator
-
     let (msg, _) = msg
         .pack_encrypted(
             &MEDIATOR_DID,
@@ -48,6 +64,7 @@ async fn mediate_request() {
         )
         .await
         .expect("Unable to pack_encrypted");
+
     let client = reqwest::Client::new();
     let response = client
         .post("http://localhost:3000/mediate")
@@ -61,7 +78,7 @@ async fn mediate_request() {
         .unwrap();
     println!("packed message from");
 
-    // unpack response
+    // Unpack response
     let (msg, _) = Message::unpack(
         &response,
         &did_resolver,
@@ -70,24 +87,25 @@ async fn mediate_request() {
     )
     .await
     .unwrap();
-  ////  ROUTING_DID = msg.body.get("r")
-
 }
+
 async fn keylist_update_payload() {
     // --- Building message from ALICE to MEDIATOR ---
     let msg = Message::build(
         "id_alice_keylist_update_request".to_owned(),
         "https://didcomm.org/coordinate-mediation/2.0/keylist-update".to_owned(),
-        json!({"updates": [
-            {
-                "recipient_did": "did:key:alice_identity_pub1@alice_mediator",
-                "action": "add"
-            },
-            {
-                "recipient_did": "did:key:alice_identity_pub2@alice_mediator",
-                "action": "remove"
-            }
-        ]}),
+        json!({
+            "updates": [
+                {
+                    "recipient_did": "did:key:alice_identity_pub1@alice_mediator",
+                    "action": "add"
+                },
+                {
+                    "recipient_did": "did:key:alice_identity_pub2@alice_mediator",
+                    "action": "remove"
+                }
+            ]
+        }),
     )
     .header("return_route".into(), json!("all"))
     .to(MEDIATOR_DID.to_owned())
@@ -113,4 +131,81 @@ async fn keylist_update_payload() {
 
     // --- Sending message by Alice ---
     println!("Edge agent is sending message \n{}\n", msg);
+
+    let client = Client::new();
+    let response = client
+        .post("http://localhost:3000/mediate")
+        .header(CONTENT_TYPE, "application/didcomm-encrypted+json")
+        .body(msg)
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    // Unpacking the message
+    let (msg, _) = Message::unpack(
+        &response,
+        &did_resolver,
+        &secrets_resolver,
+        &UnpackOptions::default(),
+    )
+    .await
+    .unwrap();
+}
+
+async fn keylist_query_payload() {
+    let client = Client::new();
+
+    // --- Building message from ALICE to MEDIATOR ---
+    let message = Message::build(
+        "id_alice_keylist_query".to_owned(),
+        "https://didcomm.org/coordinate-mediation/2.0/keylist-query".to_owned(),
+        json!({}),
+    )
+    .to(MEDIATOR_DID.to_owned())
+    .from(ALICE_DID.to_owned())
+    .finalize();
+
+    // --- Packing encrypted and authenticated message ---
+    let did_resolver =
+        ExampleDIDResolver::new(vec![ALICE_DID_DOC.clone(), MEDIATOR_DID_DOC.clone()]);
+    let secrets_resolver = ExampleSecretsResolver::new(ALICE_SECRETS.clone());
+
+    let (message, _) = message
+        .pack_encrypted(
+            &MEDIATOR_DID,
+            Some(&ALICE_DID),
+            None,
+            &did_resolver,
+            &secrets_resolver,
+            &PackEncryptedOptions::default(),
+        )
+        .await
+        .expect("Unable to pack_encrypted");
+
+    // --- Sending message by Alice ---
+    println!("Edge agent is sending message \n{}\n", message);
+
+    let response = client
+        .post("http://localhost:3000/mediate")
+        .header(CONTENT_TYPE, "application/didcomm-encrypted+json")
+        .body(message)
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    // --- Unpack the message ---
+    let (message, _) = Message::unpack(
+        &response,
+        &did_resolver,
+        &secrets_resolver,
+        &UnpackOptions::default(),
+    )
+    .await
+    .unwrap();
 }
