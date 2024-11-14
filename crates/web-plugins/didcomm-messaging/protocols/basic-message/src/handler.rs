c62e590f69bbbe7eb88c2f06bdcecccc0dbb5401
ndefokou <tcheutchouaarthur38@gmail.com>
2024-11-14 16:44:54 +0100

    fixe():adjusting the message-api test

--- crates/web-plugins/didcomm-messaging/protocols/basic-message/src/handler.rs
+++ crates/web-plugins/didcomm-messaging/protocols/basic-message/src/handler.rs
@@ -1,10 +1,10 @@
-use std::sync::Arc;
+use crate::model::BasicMessage;
 use axum::response::Response;
 use chrono::Utc;
 use serde_json::json;
 use shared::state::AppState;
+use std::sync::Arc;
 use uuid::Uuid;
-use crate::model::BasicMessage;
 
 pub async fn handle_basic_message(
     state: Arc<AppState>,
@@ -27,13 +27,27 @@ mod tests {
     use crate::model::BasicMessage;
     use chrono::Utc;
     use did_utils::didcore::Document;
+    use keystore::tests::MockKeyStore;
     use serde_json::json;
-    use shared::state::AppState;
+    use shared::{
+        repository::tests::{MockConnectionRepository, MockMessagesRepository},
+        state::{AppState, AppStateRepository},
+    };
     use std::sync::Arc;
-    
+
     #[tokio::test]
     async fn test_handle_basic_message() {
-        let app_state = Arc::new(AppState::from("".to_string(), Document::default(), None));
+        let repository = AppStateRepository {
+            connection_repository: Arc::new(MockConnectionRepository::from(vec![])),
+            message_repository: Arc::new(MockMessagesRepository::from(vec![])),
+            keystore: Arc::new(MockKeyStore::new(vec![])),
+        };
+
+        let app_state = Arc::new(AppState::from(
+            "".to_string(),
+            Document::default(),
+            Some((repository)),
+        ));
 
         let input_message = BasicMessage {
             id: Uuid::new_v4().to_string(),
@@ -48,9 +62,18 @@ mod tests {
         assert!(result.is_ok());
         let response_message = result.unwrap().unwrap();
 
-        assert_eq!(response_message.message_type, "https://didcomm.org/basicmessage/2.0/message");
+        assert_eq!(
+            response_message.message_type,
+            "https://didcomm.org/basicmessage/2.0/message"
+        );
         assert_eq!(response_message.lang, input_message.lang);
-        assert_eq!(response_message.body["content"], json!(format!("Received: {}", input_message.body["content"].as_str().unwrap())));
+        assert_eq!(
+            response_message.body["content"],
+            json!(format!(
+                "Received: {}",
+                input_message.body["content"].as_str().unwrap()
+            ))
+        );
 
         assert_ne!(response_message.id, input_message.id);
         assert!(response_message.created_time >= input_message.created_time);
